bccc0f339850d02feb79b2e9bbd96ef124de5df3
Faith <pfaithmtan@gmail.com>
2019-09-16 12:13:17 -0700

    Refactor App to dynamically render
    - Render array of grocery items
    - Utilise GroceryListItem component

--- app.js
+++ app.js
@@ -2,11 +2,15 @@
 const App = () => (
     <div>
         <h1>My Grocery List</h1>
-        <ul>
-            <Cucumbers />
-            <Kale />
-        </ul>
+        <GroceryListItem groceryItems={[<Cucumbers />, <Kale />]}/>
     </div>
+);
+
+const GroceryListItem = (props) => (
+    <ul>
+        {props.groceryItems[0]}
+        {props.groceryItems[1]}
+    </ul>
 );
 
 const Cucumbers = () => (
