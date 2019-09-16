7e39d2f40cd48e64b4e357aee4e31ddad7962350
Faith <pfaithmtan@gmail.com>
2019-09-16 12:05:53 -0700

    Create grocery list components
    - Nest Cucumber and Kale components in App component

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const App = () => (
     <div>
         <h1>My Grocery List</h1>
         <ul>
-            <li>Cucumbers</li>
-            <li>Kale</li>
+            <Cucumbers />
+            <Kale />
         </ul>
     </div>
-  );
+);
 
-  ReactDOM.render(<App />, document.getElementById("app"));
+const Cucumbers = () => (
+    <li>Cucumbers</li>
+);
+
+const Kale = () => (
+    <li>Kale</li>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
