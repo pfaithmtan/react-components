9b14385ed2d889e0b1537f98955653fd97e39255
Faith <pfaithmtan@gmail.com>
2019-09-16 12:01:12 -0700

    Render App to DOM
    - Nest unordered list to div

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // TODO
+const App = () => (
+    <div>
+        <h1>My Grocery List</h1>
+        <ul>
+            <li>Cucumbers</li>
+            <li>Kale</li>
+        </ul>
+    </div>
+  );
+
+  ReactDOM.render(<App />, document.getElementById("app"));
